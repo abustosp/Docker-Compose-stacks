d805c66db6ba72514db5763be2067c17503072d4
Agustin Bustos Piasentini <63634574+abustosp@users.noreply.github.com>
2023-07-21 22:07:11 -0300

    Nextcloud

---
+++ NextCloud/config.php
@@ -0,0 +1,42 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'oc1q4fy2q7hd',
+  'passwordsalt' => 'Ouz8TXRIYZLsxwk+Ij130IwoCnzce0',
+  'secret' => '+merd+oB202AmA3lFGO9QKumyR4LmSJkhaOUSKFrbqmTSF7N',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'Página-Propia-o-DDNS',
+    2 => '192.168.0.*',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '26.0.3.2',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mariadb',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'ContraseñaDeNextCloud',
+  'installed' => true,
+  'skeletondirectory' => '',
+);
